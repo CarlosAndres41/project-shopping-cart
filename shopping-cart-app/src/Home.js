aa2b8dbb1200f13b4cede96207374ac0b837a4a3
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 14:58:06 -0500

    Import and add Services to Home

--- shopping-cart-app/src/Home.js
+++ shopping-cart-app/src/Home.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent';
+import Services from './components/Services';
 
 function Home() {
     return (
         <div className='home'>
             <Navbar />
             <MainContent />
+            <Services />
         </div>
     );
 }
