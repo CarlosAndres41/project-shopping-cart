15117fd548ae27237b55980121ff3aa6f48565b6
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 18:35:53 -0500

    Create utilty folder and getProducts method

---
+++ shopping-cart-app/src/utils/getProducts.js
@@ -0,0 +1,9 @@
+const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products?limit=10');
+    const data = await response.json();
+    return data;
+};
+
+const products = fetchProducts();
+
+export default products;
