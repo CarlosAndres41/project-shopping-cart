a69bcf6c0760e865eabbefa0afd41eed4a9c454c
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 16:41:35 -0500

    Create initial Shop page

---
+++ shopping-cart-app/src/Shop.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Navbar from './components/Navbar';
+
+function Shop() {
+    return (
+        <div className='shop'>
+            <Navbar />
+        </div>
+    );
+}
+
+export default Shop;
