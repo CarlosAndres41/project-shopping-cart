7063308e4d11ae86d176aa772d3b0009f45248ba
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 18:47:02 -0500

    Create basic ProductCard component

---
+++ shopping-cart-app/src/components/ProductCard.js
@@ -0,0 +1,12 @@
+// Create a react component that displays a product in the shop
+// It will show title, description, price, category and image
+
+const ProductCard = () => {
+    return (
+        <div className='product-card'>
+            <h1>Product Card</h1>
+        </div>
+    );
+};
+
+export default ProductCard;
