f7737fabf2c25c8e2505a2575d90400c6ace38f9
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 17:52:33 -0500

    Finish Deals structure

--- shopping-cart-app/src/components/Deals.js
+++ shopping-cart-app/src/components/Deals.js
@@ -1,7 +1,19 @@
+import deals from '../imgs/deals.png';
+
 const Deals = () => {
     return (
-        <div>
-            <h1>Deals</h1>
+        <div className='deals'>
+            <div className='deals-banner'>
+                <div className='deals-left'>
+                    <h2>Grab Up To 50% Off On Selected Products</h2>
+                    <a href='/deals' className='deals-btn'>
+                        Shop Now
+                    </a>
+                </div>
+                <div className='deals-right'>
+                    <img src={deals} alt='deals'></img>
+                </div>
+            </div>
         </div>
     );
 };
