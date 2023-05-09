05e577011e9283bbee531b432cc10c644ea03388
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 16:43:31 -0500

    Add route to Shop

--- shopping-cart-app/src/RouteSwitch.js
+++ shopping-cart-app/src/RouteSwitch.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
+import Shop from './Shop';
 
 const RouteSwitch = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/shop' element={<Shop />} />
             </Routes>
         </BrowserRouter>
     );
