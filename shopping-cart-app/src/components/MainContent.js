1a7e19fbd015db5536ed432a576d91c5dc746957
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 01:00:56 -0500

    Add class to image

--- shopping-cart-app/src/components/MainContent.js
+++ shopping-cart-app/src/components/MainContent.js
@@ -11,6 +11,7 @@ const MainContent = () => {
             </div>
             <div className='right-content'>
                 <img
+                    className='main-image'
                     src='https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
                     alt='main'
                 />
