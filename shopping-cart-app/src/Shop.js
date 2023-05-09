139eaa160b5c90c40b689345337e57ff50874656
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 17:39:55 -0500

    Import Deals and Footer components

--- shopping-cart-app/src/Shop.js
+++ shopping-cart-app/src/Shop.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Deals from './components/Deals';
+import Footer from './components/Footer';
 
 function Shop() {
     return (
         <div className='shop'>
             <Navbar />
+            <Deals />
+            <Footer />
         </div>
     );
 }
