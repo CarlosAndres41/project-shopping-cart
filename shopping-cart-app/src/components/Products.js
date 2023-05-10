3a7d5c0ee1914e4cc9f23db1a9f5fe2d782204e7
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 20:09:36 -0500

    Generate a ProductCard for each product

--- shopping-cart-app/src/components/Products.js
+++ shopping-cart-app/src/components/Products.js
@@ -1,7 +1,15 @@
+import products from '../utils/getProducts';
+import ProductCard from './ProductCard';
+
 const Products = () => {
     return (
         <div className='products'>
             <h2>Recommended For You</h2>
+            <div className='shop-items'>
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
         </div>
     );
 };
