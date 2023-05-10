7a2048e95b60d3d0e2f2ab129875a19e61ab5b9a
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-09 20:42:07 -0500

    Add structure to display product information

--- shopping-cart-app/src/components/ProductCard.js
+++ shopping-cart-app/src/components/ProductCard.js
@@ -1,10 +1,21 @@
 // Create a react component that displays a product in the shop
 // It will show title, description, price, category and image
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+    const { title, price, description, category, image, rating } =
+        props.product;
     return (
         <div className='product-card'>
-            <h1>Product Card</h1>
+            <div className='product-image'>
+                <img src={image} alt={title} />
+            </div>
+            <div className='product-info'>
+                <div className='title-price'>
+                    <h4>{title}</h4>
+                    <h3>${price}</h3>
+                </div>
+                <h6>{category}</h6>
+            </div>
         </div>
     );
 };
