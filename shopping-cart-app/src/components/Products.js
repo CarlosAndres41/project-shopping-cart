4088e8c8f378e1d7c02b1dc0d6607e5441af2e5b
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-11 00:11:07 -0500

    Pass addToCard as prop to all Cards

--- shopping-cart-app/src/components/Products.js
+++ shopping-cart-app/src/components/Products.js
@@ -1,13 +1,19 @@
 import products from '../utils/getProducts';
 import ProductCard from './ProductCard';
 
-const Products = () => {
+const Products = (props) => {
     return (
         <div className='products'>
             <h2>Recommended For You</h2>
             <div className='shop-items'>
                 {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard
+                        key={product.id}
+                        product={product}
+                        addToCart={props.addToCart}
+                        removeFromCart={props.removeFromCart}
+                        cart={props.addToCart}
+                    />
                 ))}
             </div>
         </div>
