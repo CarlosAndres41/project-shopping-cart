16b8c7e498d8d5bf44d50e1f5ae5c153b4d619f1
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-10 23:40:15 -0500

    Add states for cart products, total and quantity

--- shopping-cart-app/src/Shop.js
+++ shopping-cart-app/src/Shop.js
@@ -4,8 +4,13 @@ import Navbar from './components/Navbar';
 import Deals from './components/Deals';
 import Products from './components/Products';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function Shop() {
+    const [cartProducts, setCartProducts] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+    const [cartQuantity, setCartQuantity] = useState(0);
+
     return (
         <div className='shop'>
             <Navbar />
