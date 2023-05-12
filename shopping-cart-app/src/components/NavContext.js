c22b80ab24f571bab4b4bf1c9daf5848560a7f46
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-11 23:50:33 -0500

    Create context with all the states and function needed for navbar

--- shopping-cart-app/src/components/NavContext.js
+++ shopping-cart-app/src/components/NavContext.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 
-const NavContext = React.createContext();
+export const MyContext = createContext();
 
-export default NavContext;
+export function MyContextProvider(props) {
+    const [cartProducts, setCartProducts] = useState([]);
+    const [cartTotal, setCartTotal] = useState(0);
+    const [cartQuantity, setCartQuantity] = useState(0);
+
+    const removeFromCart = (product) => {
+        const index = cartProducts.indexOf(product);
+        if (index !== -1) {
+            const newCartProducts = [...cartProducts];
+            newCartProducts.splice(index, 1);
+            setCartProducts(newCartProducts);
+            setCartTotal(cartTotal - product.price);
+            setCartQuantity(cartQuantity - 1);
+        }
+    };
+
+    return (
+        <MyContext.Provider
+            value={{
+                cartProducts,
+                cartTotal,
+                cartQuantity,
+                setCartProducts,
+                setCartTotal,
+                setCartQuantity,
+                removeFromCart,
+            }}
+        >
+            {props.children}
+        </MyContext.Provider>
+    );
+}
