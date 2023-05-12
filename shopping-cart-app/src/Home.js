ba27cec6244bb59a56e7276336d02ee754b4185c
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-11 23:51:01 -0500

    Use navbar props from MyContext

--- shopping-cart-app/src/Home.js
+++ shopping-cart-app/src/Home.js
@@ -1,14 +1,32 @@
 import './App.css';
 
+import React, { useContext } from 'react';
+import { MyContext } from './components/NavContext';
+
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent';
 import Services from './components/Services';
 import Footer from './components/Footer';
 
 function Home() {
+    const {
+        cartProducts,
+        cartTotal,
+        cartQuantity,
+        setCartProducts,
+        setCartTotal,
+        setCartQuantity,
+        removeFromCart,
+    } = useContext(MyContext);
+
     return (
         <div className='home'>
-            <Navbar />
+            <Navbar
+                cartProducts={cartProducts}
+                cartTotal={cartTotal}
+                cartQuantity={cartQuantity}
+                removeFromCart={removeFromCart}
+            />
             <MainContent />
             <Services />
             <Footer />
