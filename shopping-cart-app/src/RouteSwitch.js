23b4150528c22abbe26560b27d875f66699cdb85
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-12 00:11:49 -0500

    Create components for yet to be implemented routes

--- shopping-cart-app/src/RouteSwitch.js
+++ shopping-cart-app/src/RouteSwitch.js
@@ -5,6 +5,10 @@ import { MyContextProvider } from './components/NavContext';
 
 import Home from './Home';
 import Shop from './Shop';
+import NewDeals from './components/NewDeals';
+import Register from './components/Register';
+import Checkout from './components/Checkout';
+import Brand from './components/Brand';
 
 const RouteSwitch = () => {
     return (
@@ -13,6 +17,10 @@ const RouteSwitch = () => {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/shop' element={<Shop />} />
+                    <Route path='/deals' element={<NewDeals />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/checkout' element={<Checkout />} />
+                    <Route path='/brand' element={<Brand />} />
                 </Routes>
             </BrowserRouter>
         </MyContextProvider>
---
+++ shopping-cart-app/src/components/NewDeals.js
@@ -0,0 +1,9 @@
+const NewDeals = () => {
+    return (
+        <div>
+            <h1>New Deals</h1>
+        </div>
+    );
+};
+
+export default NewDeals;
