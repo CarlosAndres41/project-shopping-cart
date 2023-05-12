934be6539fa091cb89ee21ee5f58c08ec3131133
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-11 20:25:30 -0500

    Add information to be displayed in the cart

--- shopping-cart-app/src/components/CartItem.js
+++ shopping-cart-app/src/components/CartItem.js
@@ -1,5 +1,15 @@
-const CartItem = () => {
-    return <div>CartItem </div>;
+const CartItem = (props) => {
+    return (
+        <div className='item-card'>
+            <img
+                src={props.product.image}
+                alt='product'
+                className='item-image'
+            ></img>
+            <p>{props.product.title}</p>
+            <p>${props.product.price}</p>
+        </div>
+    );
 };
 
 export default CartItem;
