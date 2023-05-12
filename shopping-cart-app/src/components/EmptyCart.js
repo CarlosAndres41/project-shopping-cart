c683aa665309892827ae469cea3e0149246060eb
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-11 23:59:08 -0500

    Import Link and replace anchor tag

--- shopping-cart-app/src/components/EmptyCart.js
+++ shopping-cart-app/src/components/EmptyCart.js
@@ -1,11 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const EmptyCart = () => {
     return (
         <div className='empty-cart'>
             <h3>Your cart is empty</h3>
             <p>Add some items to your cart</p>
-            <a href='/shop' className='continue-shopping-btn'>
+            <Link to='/shop' className='continue-shopping-btn'>
                 Go Shopping
-            </a>
+            </Link>
         </div>
     );
 };
