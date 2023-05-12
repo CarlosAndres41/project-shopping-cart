54a4c7ab84c73d7d232c3be1765181da711a7016
Carlos Andrés <ca.castaneda@outlook.com>
2023-05-11 20:57:03 -0500

    Refactor remove from cart

--- shopping-cart-app/src/Shop.js
+++ shopping-cart-app/src/Shop.js
@@ -19,13 +19,27 @@ function Shop() {
         setCartQuantity(cartQuantity + 1);
     };
 
+    // const removeFromCart = (product) => {
+    //     const newCartProducts = cartProducts.filter(
+    //         (cartProduct) => cartProduct !== product
+    //     );
+    //     setCartProducts(newCartProducts);
+    //     setCartTotal(cartTotal - product.price);
+    //     setCartQuantity(cartQuantity - 1);
+    // };
+
+    // Refactor removeFromCart to use index of product to remove from cart
+    // And delete only the first instance when multiple instances of the same product are in the cart
+
     const removeFromCart = (product) => {
-        const newCartProducts = cartProducts.filter(
-            (cartProduct) => cartProduct !== product
-        );
-        setCartProducts(newCartProducts);
-        setCartTotal(cartTotal - product.price);
-        setCartQuantity(cartQuantity - 1);
+        const index = cartProducts.indexOf(product);
+        if (index !== -1) {
+            const newCartProducts = [...cartProducts];
+            newCartProducts.splice(index, 1);
+            setCartProducts(newCartProducts);
+            setCartTotal(cartTotal - product.price);
+            setCartQuantity(cartQuantity - 1);
+        }
     };
 
     const NavProps = {
